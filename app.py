02a7cfcf459708a6a6e2b8982285bb79582edf04
vishnu <vishnu.var.reddy@gmail.com>
2020-11-22 17:19:21 +0530

    Cost estimation calculator pushed

--- app.py
+++ app.py
@@ -3,27 +3,28 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def get_price(total_quantity, price_per_bag, quantity_per_bag):
+    kgs = int((total_quantity % int(total_quantity)) * 100) #8.23 --> .23
+    print(kgs/quantity_per_bag + int(total_quantity))
+    return (kgs/quantity_per_bag + int(total_quantity)) * price_per_bag
 
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+    total_quantity, price_per_bag, quantity_per_bag = [float(x) for x in request.form.values()]
+    output = get_price(total_quantity, price_per_bag, quantity_per_bag)
+    output = round(output, 3)
+    return render_template('index.html', prediction_text='Total cost for {} at {} be ₹{}'.format(total_quantity,
+                                                                                                  price_per_bag, output))
 
 if __name__ == "__main__":
     app.run(debug=True)
